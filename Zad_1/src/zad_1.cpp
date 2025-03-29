664412d60d3b9880318f60cbb424331b2c9c7f93
the-UHO <adamjablonski.important2@gmail.com>
2025-03-29 22:14:03 +0100

    Poprawki zad 1

--- Zad_1/src/zad_1.cpp
+++ Zad_1/src/zad_1.cpp
@@ -6,21 +6,15 @@
 
 
 
-/*
-*   > ph0  f0  ph1  f1  ph2  f2  ph3  f3  ph4  f4  ph5  f5 ... >
-*   
-*/
-
-
-
-
-#define MAX_ITERATIONS 100
-#define INFINITE_ITERATIONS false
-#define SLEEP_TIME 100
-
 int philosophersNum;
+int sleepTime;
+bool printForkLogsFLAG;
+int maxIterations;
+bool infiniteIterationsFLAG;
+
 pthread_mutex_t* forks;
-pthread_t* philosopherThreads;
+pthread_t* threadIDs;
+
 
 void* philosopherThread(void* thdata){
     int timesEaten = 0;
@@ -43,39 +37,35 @@ void* philosopherThread(void* thdata){
     printf("Hello. I'm Philosopher %d in a thread nr %llu. I can reach leftFork: %d and rightFork: %d \n", philosopherID, pthread_self(), firstFork, secondFork);
 
 
-    for (int iter = 0; iter < MAX_ITERATIONS || INFINITE_ITERATIONS ; iter++)
+    for (int iter = 0; iter < maxIterations || infiniteIterationsFLAG ; iter++)
     {
         //Think
         printf("Philosopher %d is thinking for %d. time.\n", philosopherID, iter);
-        usleep(SLEEP_TIME);
+        usleep(sleepTime);
 
         //Try to get forks
         printf("Philosopher %d is trying to eat for %d. time.\n", philosopherID, iter);
         pthread_mutex_lock(&forks[firstFork]);
-        printf("Fork %d picked up by Ph.%d.\n", firstFork, philosopherID);
+        if (printForkLogsFLAG) printf("Fork %d picked up by Ph.%d.\n", firstFork, philosopherID);
         pthread_mutex_lock(&forks[secondFork]);
-        printf("Fork %d picked up by Ph.%d.\n", secondFork, philosopherID);
+        if (printForkLogsFLAG) printf("Fork %d picked up by Ph.%d.\n", secondFork, philosopherID);
         
         //Eat
         printf("Philosopher %d is eating for %d. time.\n", philosopherID, iter);
-        usleep(2*SLEEP_TIME);
+        usleep(2*sleepTime);
         timesEaten++;
 
         //Give forks back
         pthread_mutex_unlock(&forks[firstFork]);
-        printf("Fork %d put down by Ph.%d.\n", firstFork, philosopherID);
+        if (printForkLogsFLAG) printf("Fork %d put down by Ph.%d.\n", firstFork, philosopherID);
         pthread_mutex_unlock(&forks[secondFork]);
-        printf("Fork %d put down by Ph.%d.\n", secondFork, philosopherID);
+        if (printForkLogsFLAG) printf("Fork %d put down by Ph.%d.\n", secondFork, philosopherID);
         printf("Philosopher %d finished eating. for %d. time\n", philosopherID, iter);
     }
     
     
-    printf("\nEnding thread %llu - philosopher %d has eaten %d times.============================================================\n", pthread_self(), philosopherID, timesEaten);
-    printf("\nEnding thread %llu - philosopher %d has eaten %d times.============================================================\n", pthread_self(), philosopherID, timesEaten);
-    printf("\nEnding thread %llu - philosopher %d has eaten %d times.============================================================\n", pthread_self(), philosopherID, timesEaten);
-    printf("\nEnding thread %llu - philosopher %d has eaten %d times.============================================================\n", pthread_self(), philosopherID, timesEaten);
-    printf("\nEnding thread %llu - philosopher %d has eaten %d times.============================================================\n", pthread_self(), philosopherID, timesEaten);
-
+    printf("\nEnding thread %llu - philosopher %d has eaten %d times. \t ===== \n", pthread_self(), philosopherID, timesEaten);
+    
     int* eatenPtr = new int(timesEaten);
     pthread_exit((void *) eatenPtr);
 }
@@ -83,26 +73,49 @@ void* philosopherThread(void* thdata){
 
 int main(int argc, char* argv[])
 {
+    sleepTime = 1000;
+    infiniteIterationsFLAG = false;
     int rt;
-    pthread_t* threadID;
-    if (argc < 2){
-        printf("Please provide number of philosophers as a program argument.\n");
+
+    if (argc < 5){
+        printf(
+            "\n ERROR: Not enough program arguments.\n"
+            " > Please provide arguments as follows : /PROGRAM_NAME/ " 
+            "<Number of philosophers (as int)> <Number of milisec. that philosopher will think (as int)> <Number of iterations per philosopher (as int, '-1' if infinite)> <Flag for writing fork logs (as 0 or 1)>\n");
         exit(1);
     }
 
     philosophersNum = atoi(argv[1]);
+    sleepTime *= atoi(argv[2]);
+    maxIterations = atoi(argv[3]);
+    printForkLogsFLAG = (bool)atoi(argv[4]);
+    if (maxIterations == -1) infiniteIterationsFLAG = true;
+    
+
     if (philosophersNum < 1){
-        printf("Provided invalid argument : %s \n", argv[1]);
+        printf("\n ERROR : Provided invalid number of philosophers : %s \n", argv[1]);
         exit(2);
     }
     else if (philosophersNum < 2){
-        printf("Number of philosophers must be greater than 1\n");
+        printf("\n ERROR : Number of philosophers must be greater than 1\n");
+        exit(2);
+    }
+    if (sleepTime < 1000){
+        printf("\n ERROR : Provided invalid number of miliseconds : %s \n", argv[2]);
         exit(3);
     }
+    if (maxIterations == 0 || maxIterations < -1 ){
+        printf("\n ERROR : Provided invalid number of iterations : %s \n", argv[3]);
+        exit(4);
+    }
+    
+
+
+
     
     
     forks = new pthread_mutex_t[philosophersNum];
-    threadID = new pthread_t[philosophersNum];
+    threadIDs = new pthread_t[philosophersNum];
 
     for (int i = 0; i < philosophersNum; i++){
         pthread_mutex_init(&forks[i], NULL);
@@ -110,10 +123,10 @@ int main(int argc, char* argv[])
     
     for (int i = 0; i < philosophersNum; i++){
         int* philosopherID = new int(i);
-        rt = pthread_create(&threadID[i], NULL, philosopherThread, (void*)philosopherID);
+        rt = pthread_create(&threadIDs[i], NULL, philosopherThread, (void*)philosopherID);
         if (rt){
-            printf("\n ERROR: return code from pthread_create : %d - For iteration : %d \n", rt, i);
-            exit(4);
+            printf("\n ERROR: Return code from pthread_create : %d - For iteration : %d \n", rt, i);
+            exit(10);
         }
         
     }
@@ -123,18 +136,22 @@ int main(int argc, char* argv[])
     void* thStatus;
     for (int i = 0; i < philosophersNum; i++)
     {
-        pthread_join(threadID[i], &thStatus);
-        printf("Thread %llu joined with status : %d \n", threadID[i], *(int*)thStatus);
+        rt = pthread_join(threadIDs[i], &thStatus);
+        if (rt){
+            printf("\n ERROR: Return code from pthread_join : %d \n", rt);
+            exit(11);
+        }
+        printf("Thread %llu joined with status : %d \n", threadIDs[i], *(int*)thStatus);
     }
 
     
 
-    printf("\nEnding main.\n");
+    printf("\nEnding main thread.\n");
     for (int i = 0; i < philosophersNum; i++)
     {
         pthread_mutex_destroy(&forks[i]);
     }
     delete[] forks;
-    delete[] philosopherThreads;
-    pthread_exit(NULL);//---
+    delete[] threadIDs;
+    pthread_exit(NULL);
 }
